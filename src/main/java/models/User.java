692d48d8ab1adc6035a65d8763e95dd0a4efe14c
melissa kobia <melissakobia1997@gmail.com>
2019-10-28 04:36:34 +0300

    Adds Users class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,56 @@
 package models;
 
 public class User {
+    private String username;
+    private String position;
+    private String role;
+    private int id;
+    private int departmentId;
+
+    public User(String username, String position, String role, int departmentId) {
+        this.username = username;
+        this.position = position;
+        this.role = role;
+        this.departmentId = departmentId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
 }
