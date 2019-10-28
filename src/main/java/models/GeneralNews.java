99e768ff7da818c3486f81651988175b67ffb364
melissa kobia <melissakobia1997@gmail.com>
2019-10-28 04:40:43 +0300

    Adds a GeneralNews class that inherits from News Abstract class

--- src/main/java/models/GeneralNews.java
+++ src/main/java/models/GeneralNews.java
@@ -1,4 +1,10 @@
 package models;
 
 public class GeneralNews extends News {
+    public static final String DATABASE_TYPE = "general";
+
+    public GeneralNews(String content) {
+        this.content = content;
+        type = DATABASE_TYPE;
+    }
 }
